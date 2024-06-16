c11724488fcfd3d0e7237e83f69694c209ef2e08
rasmussvala <rassv453@student.liu.se>
2024-06-16 18:10:32 +0200

    wip

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static void foo(vector<int>& nums) {
+  for (int i = 0; i < nums.size() - 1; i++) {
+    // Move to the end
+    if (nums[i] == 0) {
+      for (int j = i; j < nums.size() - 1; j++) {
+        swap(nums[j], nums[j + 1]);
+      }
+    }
+  }
+}
 
 int main() {
-  int result = foo();
+  vector<int> nums = {0, 0, 1};
+  foo(nums);
 
   return 0;
 }
