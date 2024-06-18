ca2c35405bab915e25dcc9f5b719babcaa79910e
rasmussvala <rasmus.svala@hotmail.com>
2024-06-18 22:17:43 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(vector<int>& nums) {
+  int negativeCount = 0;
+  for (const auto& n : nums) {
+    if (n == 0) return 0;
+    if (n < 0) ++negativeCount;
+  }
+
+  return (negativeCount % 2 == 0) ? 1 : -1;
+}
 
 int main() {
-  int result = foo();
+  vector<int> nums = {-1, 1, -1, 1, -1};
+  int result = foo(nums);
 
   return 0;
 }
