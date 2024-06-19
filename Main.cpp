7f1b2a69f73007f23966f7fc70d67ee704dae0e3
rasmussvala <rasmus.svala@hotmail.com>
2024-06-19 18:16:06 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(int n) {
+  if (n <= 1) return n;
+
+  vector<int> myVec(n + 1);
+  myVec[0] = 0;
+  myVec[1] = 1;
+
+  for (int i = 2; i < n + 1; ++i) {
+    myVec[i] = myVec[i - 1] + myVec[i - 2];
+  }
+
+  return myVec[n];
+}
 
 int main() {
-  int result = foo();
+  int n = 0;
+  int result = foo(n);
 
   return 0;
 }
