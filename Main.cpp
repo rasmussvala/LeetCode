293f2c6ba8be5181180456543eb692e9d5954008
rasmussvala <rasmus.svala@hotmail.com>
2024-06-21 13:49:59 +0200

    solved 58

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,25 @@
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(string s) {
+  int counter = 0;
+  int lastValue = 0;
+  for (const auto& l : s) {
+    // 32 is the unicode for space or empty idk
+    if (l == 32) {
+      counter = 0;
+    } else {
+      ++counter;
+      lastValue = counter;
+    }
+  }
+
+  return lastValue;
+}
 
 int main() {
-  int result = foo();
+  string s = "hello world";
+  int result = foo(s);
 
   return 0;
 }
