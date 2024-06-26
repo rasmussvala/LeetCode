f47bfc3b76c207396eba23a1e58f1ac601887b94
rasmussvala <rasmus.svala@hotmail.com>
2024-06-26 17:45:15 +0200

    wip

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(string s, string t) {
+  int n = s.size();
+  int diff = 0;
+  int sum = 0;
+
+  for (int i = 0; i < n; ++i) {
+    diff = s[i] - t[i];
+    sum += abs(diff);
+  }
+
+  return sum;
+}
 
 int main() {
-  int result = foo();
+  string s = "rwohu";
+  string t = "rwuoh";
+  int result = foo(s, t);
 
   return 0;
 }
