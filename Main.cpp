0daed20d9930fa395c247317beb7a107872b6c97
rasmussvala <rasmus.svala@hotmail.com>
2024-06-27 22:57:22 +0200

    solved 3146

--- Main.cpp
+++ Main.cpp
@@ -6,9 +6,11 @@ static int foo(string s, string t) {
   int n = s.size();
   int diff = 0;
   int sum = 0;
+  int index = -1;
 
   for (int i = 0; i < n; ++i) {
-    diff = s[i] - t[i];
+    index = t.find(s[i]);
+    diff = i - index;
     sum += abs(diff);
   }
 
