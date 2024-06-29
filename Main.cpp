e9351e2c8ede301615faf0cfb8ebad4922746ee8
rasmussvala <rasmus.svala@hotmail.com>
2024-06-29 09:56:03 +0200

    solved 13

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(string s) {
+  unordered_map<char, int> m;
+
+  m['I'] = 1;
+  m['V'] = 5;
+  m['X'] = 10;
+  m['L'] = 50;
+  m['C'] = 100;
+  m['D'] = 500;
+  m['M'] = 1000;
+
+  int sum = 0;
+
+  for (size_t i = 0; i < s.length(); i++) {
+    if (m[s[i]] < m[s[i + 1]])
+      sum -= m[s[i]];
+    else
+      sum += m[s[i]];
+  }
+  return sum;
+}
 
 int main() {
-  int result = foo();
+  string s = "XV";
+  int result = foo(s);
 
   return 0;
 }
