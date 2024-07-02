4486bea7c7b7e5bc26b097d52062232a01142184
rasmussvala <rasmus.svala@hotmail.com>
2024-07-02 17:22:47 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static vector<int> foo(vector<int>& nums1, vector<int>& nums2) {
+  vector<int> intersect;
+  for (auto n1 : nums1) {
+    for (auto it = nums2.begin(); it != nums2.end();) {
+      if (n1 == *it) {
+        intersect.push_back(*it);
+        nums2.erase(it);
+        break;
+      } else
+        it++;
+    }
+  }
+  return intersect;
+}
 
 int main() {
-  int result = foo();
+  vector<int> nums1 = {1, 2, 2, 1};
+  vector<int> nums2 = {2, 1};
+  vector<int> result = foo(nums1, nums2);
 
   return 0;
 }
