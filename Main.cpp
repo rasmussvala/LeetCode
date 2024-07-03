067f6e2e401f784c860166ceb648323c128020a4
rasmussvala <rasmus.svala@hotmail.com>
2024-07-03 19:43:25 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,32 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(vector<int>& nums) {
+  int n = nums.size();
+  if (n <= 4) return 0;
+
+  sort(nums.begin(), nums.end());
+
+  /*
+   * We have 4 plans:
+   *
+   * kill 3 biggest elements
+   * kill 2 biggest elements + 1 smallest elements
+   * kill 1 biggest elements + 2 smallest elements
+   * kill 3 smallest elements
+   *
+   * We can now create a vector of these for cases and return the smallest
+   */
+  return min({nums[n - 1] - nums[3], nums[n - 2] - nums[2],
+              nums[n - 3] - nums[1], nums[n - 4] - nums[0]});
+}
 
 int main() {
-  int result = foo();
+  vector<int> nums = {1, 5, 0, 10, 14};
+  int result = foo(nums);
 
   return 0;
 }
