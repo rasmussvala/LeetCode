e71b1ce641976ce1649e34da3cbe17126aaf9bbd
rasmussvala <rasmus.svala@hotmail.com>
2024-07-06 17:01:54 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,25 @@
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(int n, int time) {
+  int full_loops = time / (n - 1);
+  int rest = time % (n - 1);
+
+  // we go forward
+  if (full_loops % 2 == 0) return 1 + rest;
+
+  // we go backward
+  else
+    return n - rest;
+}
 
 int main() {
-  int result = foo();
+  int n = 4;
+  int time = 5;
+
+  int result = foo(n, time);
 
   return 0;
 }
