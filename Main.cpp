c7861d0129057e5af2a0a9e3c73a3801b17015e0
rasmussvala <rasmus.svala@hotmail.com>
2024-07-07 09:16:10 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(int numBottles, int numExchange) {
+  int sum = numBottles;
+
+  while (numBottles >= numExchange) {
+    int newBottles = numBottles / numExchange;
+    int rest = numBottles % numExchange;
+    sum += newBottles;
+    numBottles = newBottles + rest;
+  }
+
+  return sum;
+}
 
 int main() {
-  int result = foo();
+  int numBottles = 15;
+  int numExchange = 4;
+  int result = foo(numBottles, numExchange);
 
   return 0;
 }
