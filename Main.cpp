18119ea63b3ae2fe25704587093ed4c811c67dcf
rasmussvala <rasmus.svala@hotmail.com>
2024-07-10 22:32:01 +0200

    solved
    test comment

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(vector<string>& logs) {
+  int depth = 0;
+
+  for (const auto& l : logs) {
+    if (l == "./")
+      continue;
+    else if (l == "../" && depth != 0)
+      --depth;
+    else if (l[0] != '.')
+      ++depth;
+  }
+
+  return depth;
+}
 
 int main() {
-  int result = foo();
+  vector<string> logs = {"d1/", "d2/", "../", "d21/", "./"};
+  int result = foo(logs);
 
   return 0;
 }
