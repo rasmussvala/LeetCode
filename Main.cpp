d822966597e1b7845baeb799ebdd24bca67b07bb
rasmussvala <rasmus.svala@hotmail.com>
2024-07-11 11:54:45 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
-static int foo() { return 0; }
+static string foo(string s) {
+  stack<char> stack;
+
+  for (auto c : s) {
+    string reverse = "";
+    if (c == ')') {
+      // Reverse the the word within the bracket
+      while (stack.top() != '(') {
+        reverse += stack.top();
+        stack.pop();
+      }
+      // Remove the opening '('
+      stack.pop();
+
+      // Once reversed add it back to the stack
+      for (auto r : reverse) stack.push(r);
+
+    } else {
+      stack.push(c);
+    }
+  }
+
+  string result = "";
+  while (!stack.empty()) {
+    result = stack.top() + result;
+    stack.pop();
+  }
+  return result;
+}
 
 int main() {
-  int result = foo();
+  string s = "(u(love)i)";
+  string result = foo(s);
 
   return 0;
 }
