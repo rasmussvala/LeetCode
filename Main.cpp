6b3a199b72a550635530a694e84ffd5fa746fcb1
rasmussvala <rasmus.svala@hotmail.com>
2024-07-14 11:23:42 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static int foo(vector<int>& nums) {
+  int counter = 0;
+
+  for (const auto& n : nums) {
+    if (n % 3 != 0) ++counter;
+  }
+
+  return counter;
+}
 
 int main() {
-  int result = foo();
+  vector<int> nums = {1, 2, 3, 4};
+  int result = foo(nums);
 
   return 0;
 }
