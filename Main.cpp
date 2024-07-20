cf955cdc1af4cf54b4688a9c148b8a730f9c198f
rasmussvala <rasmus.svala@hotmail.com>
2024-07-20 17:57:55 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static bool foo(vector<int>& flowerbed, int n) {
+  int counter = 0;
+  for (int i = 0; i < flowerbed.size(); i++) {
+    if (flowerbed[i] == 0) {
+      bool emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+      bool emptyRightPlot =
+          (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
+
+      // If both plots are empty, we can plant a flower here.
+      if (emptyLeftPlot && emptyRightPlot) {
+        flowerbed[i] = 1;
+        counter++;
+        if (counter >= n) return true;
+      }
+    }
+  }
+  return counter >= n;
+}
 
 int main() {
-  int result = foo();
+  vector<int> flowerbed = {1, 0, 0, 0, 0, 1};
+  int n = 2;
+  cout << foo(flowerbed, n);
 
   return 0;
 }
