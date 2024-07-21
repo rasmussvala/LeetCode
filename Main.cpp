00b9f44705d7d1c5e58549860fca7fe845a35840
rasmussvala <rasmus.svala@hotmail.com>
2024-07-21 20:33:02 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-static int foo() { return 0; }
+static bool isvowel(char v) {
+  unordered_map<char, bool> vowels = {
+      {'a', true}, {'e', true}, {'i', true}, {'o', true}, {'u', true},
+      {'A', true}, {'E', true}, {'I', true}, {'O', true}, {'U', true}};
+
+  return vowels.find(v) != vowels.end();
+}
+
+static string foo(string s) {
+  vector<int> indices;
+
+  // Find all indices of vowels
+  for (int i = 0; i < s.length(); ++i)
+    if (isvowel(s[i])) indices.push_back(i);
+
+  for (int i = 0; i < indices.size() / 2; i++) {
+    char temp = 0;
+    int min = indices[i];
+    int max = indices[indices.size() - 1 - i];
+
+    if (min > max) break;
+
+    temp = s[min];
+    s[min] = s[max];
+    s[max] = temp;
+  }
+
+  return s;
+}
 
 int main() {
-  int result = foo();
+  string s = "Egad! Loretta has Adams as mad as a hatter. Old age!";
+  cout << foo(s);
 
   return 0;
 }
