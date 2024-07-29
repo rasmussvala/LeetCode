fb07f0224101d6a85661eb12113f7d20ead9077f
rasmussvala <rasmus.svala@hotmail.com>
2024-07-29 22:25:23 +0200

    solved

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
-static int foo() { return 0; }
+static string foo(string s) {
+  stack<string> my_stack;
+
+  string word = "";
+  for (auto it = s.begin(); it != s.end(); ++it) {
+    char l = *it;
+    // We have a word we want to add to stack
+    if (l == ' ' and word != "") {
+      my_stack.push(word);
+      word = "";
+    }
+
+    // We are at the end and need to add the last word
+    else if (it == s.end() - 1 and l != ' ') {
+      word += l;
+      my_stack.push(word);
+    }
+
+    // We have no more word to add, only white space
+    else if (l == ' ' and word == "") {
+      continue;
+    }
+
+    // We have a char we can add to word
+    else {
+      word += l;
+    }
+  }
+
+  string result = "";
+  while (!my_stack.empty()) {
+    result += my_stack.top() + " ";
+    my_stack.pop();
+  }
+
+  // Remove the last white space
+  result.pop_back();
+
+  return result;
+}
 
 int main() {
-  cout << foo();
+  string s = "the sky is blue";
+  cout << foo(s);
 
   return 0;
 }
